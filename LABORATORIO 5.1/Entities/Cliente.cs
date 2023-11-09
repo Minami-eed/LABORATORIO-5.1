58832b14e13b88ada5dbc9501240b7289d96017c
Minami <106195391+Minami-eed@users.noreply.github.com>
2023-11-09 17:43:41 -0500

    Add Entities

---
+++ LABORATORIO 5.1/Entities/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace LABORATORIO_5._1.Entities
+{
+    public class Cliente
+    {
+        public string ? idcliente { get; set; }
+        public string ? nombrecia { get; set; }
+        public string ? direccion { get; set;}
+        public string ? idpais { get; set; }
+        public string ? telofono { get; set; }
+    }
+}
---
+++ LABORATORIO 5.1/Entities/Pais.cs
@@ -0,0 +1,8 @@
+﻿namespace LABORATORIO_5._1.Entities
+{
+    public class Pais
+    {
+        public string ? Idpais { get; set; }
+        public string ? Nombrepais { get; set; }
+    }
+}
