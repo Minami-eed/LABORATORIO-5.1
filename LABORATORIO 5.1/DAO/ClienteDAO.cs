d89fd07157662fa0ed5178d06ebb1ce15e78dc66
Minami <106195391+Minami-eed@users.noreply.github.com>
2023-11-09 19:18:28 -0500

    Add DAO

---
+++ LABORATORIO 5.1/DAO/ClienteDAO.cs
@@ -0,0 +1,96 @@
+﻿/* Importar librerias */
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using LABORATORIO_5._1.Entities;
+namespace LABORATORIO_5._1.DAO
+{
+    public class ClienteDAO
+    {
+        private string cadena;
+
+        public ClienteDAO()
+        {
+            cadena = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("cn");
+        }
+
+        public IEnumerable<Cliente> GetClientes() 
+        {
+            List<Cliente> temporal = new List<Cliente>();
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("exec usp_clientes", cn);
+                cn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    temporal.Add(new Cliente()
+                    {
+                        idcliente = dr.GetString(0),
+                        nombrecia = dr.GetString(1),
+                        direccion = dr.GetString(2),
+                        idpais = dr.GetString(3),
+                        telofono = dr.GetString(4)
+                    });
+                }
+            }
+            return temporal;
+        }
+
+        /* Método Agregar, retorna de mensaje del proceso */
+        public string Agregar(Cliente reg)
+        {
+            string mensaje = "";
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("usp_Agregar_Cliente", cn);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@cod", reg.idcliente);
+                    cmd.Parameters.AddWithValue("@nombre", reg.nombrecia);
+                    cmd.Parameters.AddWithValue("@direecion", reg.direccion);
+                    cmd.Parameters.AddWithValue("@idpais", reg.idpais);
+                    cmd.Parameters.AddWithValue("@fono", reg.telofono);
+                    cn.Open();
+                    int c = cmd.ExecuteNonQuery();
+                    mensaje = $"Se ha agregado {c} cliente";
+                }
+                catch (Exception ex)
+                {
+                    mensaje = ex.Message;
+                }
+            }
+            return mensaje;
+        }
+
+        /* Método Actualizar, retorna de mensaje del proceso */
+        public string Actualizar(Cliente reg)
+        {
+            string mensaje = "";
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("usp_Actualizar_Cliente", cn);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@cod", reg.idcliente);
+                    cmd.Parameters.AddWithValue("@nombre", reg.nombrecia);
+                    cmd.Parameters.AddWithValue("@direccion", reg.direccion);
+                    cmd.Parameters.AddWithValue("@idpais", reg.idpais);
+                    cmd.Parameters.AddWithValue("@fono", reg.telofono);
+                    cn.Open();
+                    int c = cmd.ExecuteNonQuery();
+                    mensaje = $"Se ha agregado {c} cliente";
+                }
+                catch (Exception ex)
+                {
+                    mensaje = ex.Message;
+                }
+            }
+            return mensaje;
+        }
+    }
+}
---
+++ LABORATORIO 5.1/DAO/PaisDAO.cs
@@ -0,0 +1,43 @@
+﻿/* Importar librerias */
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using LABORATORIO_5._1.Entities;
+namespace LABORATORIO_5._1.DAO
+{
+    public class PaisDAO
+    {
+        /*  
+            En el cuerpo de la clase, defina una variable cadena la cual almacena el contenido de la conexión 
+            almacenada en appsettings.json, tal como se muestra.
+         */
+        private string cadena; /* Almacena la coneccion */
+
+        public PaisDAO()
+        {
+            cadena = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("cn");
+        }
+
+        /* Metodo GetPaises */
+        public IEnumerable<Pais> GetPaises() 
+        {
+            List<Pais> temporal = new List<Pais>();
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                SqlCommand cmd = new SqlCommand("exec usp_paises", cn);
+                cn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    temporal.Add(new Pais()
+                    {
+                        Idpais = dr.GetString(0),
+                        Nombrepais = dr.GetString(1)
+                    });
+                }
+            }
+            return temporal;
+        }
+
+    }
+}
